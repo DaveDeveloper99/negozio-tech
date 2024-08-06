f325ab6e1876499ce2a6d655e55942c13eaf4367
Michel <151149656+mikzan@users.noreply.github.com>
2024-08-06 12:00:27 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,40 +1,40 @@
 $("#nav-link-pc").on("click", function(){
     $("#pc").removeClass("notvisible");
-    $("#pc").addClass("active");
+    $("#nav-link-pc").addClass("active");
     $("#smartphone").addClass("notvisible");
-    $("#smartphone").removeClass("active");
+    $("#nav-link-smartphone").removeClass("active");
     $("#photocamera").addClass("notvisible");
-    $("#photocamera").removeClass("active");
+    $("#nav-link-photocamera").removeClass("active");
     $("#TV").addClass("notvisible");
-    $("#TV").removeClass("active");
+    $("#nav-link-TV").removeClass("active");
 });
 $("#nav-link-smartphone").on("click", function(){
     $("#smartphone").removeClass("notvisible");
-    $("#smartphone").addClass("active");
+    $("#nav-link-smartphone").addClass("active");
     $("#pc").addClass("notvisible");
-    $("#pc").removeClass("active");
+    $("#nav-link-pc").removeClass("active");
     $("#photocamera").addClass("notvisible");
-    $("#photocamera").removeClass("active");
+    $("#nav-link-photocamera").removeClass("active");
     $("#TV").addClass("notvisible");
-    $("#TV").removeClass("active");
+    $("#nav-link-TV").removeClass("active");
 });
 $("#nav-link-photocamera").on("click", function(){
     $("#photocamera").removeClass("notvisible");
-    $("#photocamera").addClass("active");
+    $("#nav-link-photocamera").addClass("active");
     $("#smartphone").addClass("notvisible");
-    $("#smartphone").removeClass("active");
+    $("#nav-link-smartphone").removeClass("active");
     $("#pc").addClass("notvisible");
-    $("#pc").removeClass("active");
+    $("#nav-link-pc").removeClass("active");
     $("#TV").addClass("notvisible");
-    $("#TV").removeClass("active");
+    $("#nav-link-TV").removeClass("active");
 });
 $("#nav-link-TV").on("click", function(){
     $("#TV").removeClass("notvisible");
-    $("#TV").addClass("active");
+    $("#nav-link-TV").addClass("active");
     $("#smartphone").addClass("notvisible");
-    $("#smartphone").removeClass("active");
+    $("#nav-link-smartphone").removeClass("active");
     $("#photocamera").addClass("notvisible");
-    $("#photocamera").removeClass("active");
+    $("#nav-link-photocamera").removeClass("active");
     $("#pc").addClass("notvisible");
-    $("#pc").removeClass("active");
+    $("#nav-link-pc").removeClass("active");
 });
