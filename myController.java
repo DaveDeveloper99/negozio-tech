dc09f86d2dee0004dcb984a69ab6a8850658cc4a
DaveDeveloper99 <davide.bernabei@virgilio.it>
2024-08-06 09:08:01 +0200

    Add files via upload

---
+++ myController.java
@@ -0,0 +1,82 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.mail.MessagingException;
+
+@Controller
+public class myController {
+    
+	
+	//LISTA DI PRODOTTI ORDIANATI
+	ArrayList<Ordini> listaO = new ArrayList();
+	double sommaOrdini= 0;
+	
+	//LISTA IMMAGINI
+	ArrayList<String> listaI = new ArrayList();
+	
+	
+	                       //INTERAZIONE CON IL DATABASE
+                           private final ProdJDBCTemplate prodJDBCTemp;
+
+
+
+	                       //COSTRUTTORE CHE INIETTA ProdJDBCTEmplate.
+	                       @Autowired
+	                       public myController(ProdJDBCTemplate prodJDBCTemp) {
+		                         this.prodJDBCTemp= prodJDBCTemp;
+	                       }
+	
+	
+	
+	                      // RECUPERO LISTA ORDINI E LORO AGGIUNTA AL MODELLO
+	                      @GetMapping("/")
+	                      public String getNegozio(Model model) {
+	                             ArrayList<prodotti> lista = prodJDBCTemp.getProdotti();
+	                             
+	                             listaI.clear();
+	                             listaO.clear();
+	                             
+	                             sommaOrdini = 0;
+	               
+	                                        model.addAttribute("lista", lista);
+	                                        return "index"; //INSERISCI LA PAGINA HTML PRINCIPALE DELLO STORE
+	
+	                     }
+	                      
+	                      
+	                      // MAPPATURA PER LA GESTIONE DI PRODOTTI IN INGRESSO MAGAZZINO
+	                      @GetMapping("/gestione")
+	                      public String getGestione(@RequestParam(value = "message", required = false) String message, Model model) {
+	                          if (message != null) {
+	                              model.addAttribute("message", message);
+	                          }
+	                          return "InsProdotti";  // INSERISCI LA PAGINA HTML PER LA GESTIONE DEI PRODOTTI IN INGRESSO
+	                      }
+	                      
+	                      
+	                      
+	                      @PostMapping("/submit")
+	              		  public ResponseEntity<String> getSubmit(@RequestParam("nome") String nome, 
+	              				                                @RequestParam("descrizione") String descrizione,
+	              				                                @RequestParam("prezzo") double prezzo, 
+	              				                                @RequestParam("img") String img,
+	              				                                @RequestParam("qnt") int qnt) {
+	              			
+	              			prodJDBCTemp.ins_Prodotto(nome, descrizione, prezzo, img, qnt);
+	              			
+	              			return ResponseEntity.ok("Prodotto " +  nome  + " aggiunto con successo!");
+
+	                  
+	                  }
+	
+}
